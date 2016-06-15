5defdb89f533651361bbeab5e4c9070d3246f329
ahmadroihan <ahmadroihanmlg@gmail.com>
2016-06-15 22:21:57 +0700

    #rabu

--- app/src/main/java/com/roihan/android/MainActivity.java
+++ app/src/main/java/com/roihan/android/MainActivity.java
@@ -2,12 +2,32 @@ package com.roihan.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtChange = (TextView) findViewById(R.id.kematian);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtChange.setText("ketakutan");
+            }
+        });
+
     }
 }
+
+
+
+
+
